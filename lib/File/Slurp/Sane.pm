f96afa083978b7a902cf45b4e28971fffaa4a905
Leon Timmermans <fawaka@gmail.com>
2014-05-24 16:31:18 +0000

    Import of LEONT/File-Slurp-Sane-0.001 from CPAN.
    gitpan-cpan-distribution: File-Slurp-Sane
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         LEONT/File-Slurp-Sane-0.001.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

---
+++ lib/File/Slurp/Sane.pm
@@ -0,0 +1,176 @@
+package File::Slurp::Sane;
+$File::Slurp::Sane::VERSION = '0.001';
+use strict;
+use warnings;
+
+use Carp 'croak';
+use Encode 'resolve_alias';
+use Exporter 5.57 'import';
+use File::Spec::Functions 'catfile';
+use FileHandle;
+our @EXPORT_OK = qw/read_binary read_text read_lines read_dir/;
+
+sub read_binary {
+	my $filename = shift;
+	my $buf;
+
+	open my $fh, '<:unix', $filename or croak "Couldn't open $filename: $!";
+	if (my $size = -s $fh) {
+		my ($pos, $read) = 0;
+		do {
+			defined($read = read $fh, $buf, $size - $pos, $pos) or croak "Couldn't read $filename: $!";
+			$pos += $read;
+		} while ($read && $pos < $size);
+		return $buf;
+	}
+	else {
+		return do { local $/; <$fh> };
+	}
+}
+
+my $crlf_default = $^O eq 'MSWin32' ? 1 : 0;
+my $has_utf8_strict = eval { require PerlIO::utf8_strict };
+
+sub _text_layers {
+	my ($encoding, $options) = @_;
+	my $crlf = exists $options->{crlf} ? !! delete $options->{crlf} : $crlf_default;
+	if ($encoding =~ /^(latin|iso-8859-)1$/i) {
+		return $crlf ? ':unix:crlf:perlio' : ':raw'
+	}
+	elsif ($has_utf8_strict && $encoding =~ /^utf-?8\b/) {
+		return $crlf ? ':unix:utf8_strict:crlf:perlio' : ':unix:utf8_strict:perlio';
+	}
+	else {
+		return $crlf ? ":unix:perlio:encoding($encoding):crlf:perlio" : ":raw:encoding($encoding)";
+	}
+}
+
+sub read_text {
+	my ($filename, $encoding, %options) = @_;
+	$encoding ||= 'utf-8';
+	my $layer = _text_layers($encoding, \%options);
+	return read_binary($filename) if $layer eq ':raw';
+
+	open my $fh, "<$layer", $filename or croak "Couldn't open $filename: $!";
+	return do { local $/; <$fh> };
+}
+
+sub read_lines {
+	my ($filename, $encoding, %options) = @_;
+	$encoding ||= 'utf-8';
+	my $layer = _text_layers($encoding, \%options);
+
+	open my $fh, "<$layer", $filename or croak "Couldn't open $filename: $!";
+	return <$fh> if not %options;
+	my @buf = <$fh>;
+	close $fh;
+	chomp @buf if $options{chomp};
+	return @buf;
+}
+
+sub read_dir {
+	my ($dirname, %options) = @_;
+	opendir my ($dir), $dirname or croak "Could not open $dirname: $!";
+	my @ret = grep { not m/ \A \.\.? \z /x } readdir $dir;
+	@ret = map { catfile($dirname, $_) } @ret if $options{prefix};
+	closedir $dir;
+	return @ret;
+}
+
+1;
+
+# ABSTRACT: A simple, sane and efficient file slurper
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+File::Slurp::Sane - A simple, sane and efficient file slurper
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SYNOPSIS
+
+ use File::Slurp::Sane 'read_text';
+ my $content = read_text($filename);
+
+=head1 DESCRIPTION
+
+B<DISCLAIMER>: this module is experimental, and may still change in non-compatible ways.
+
+This module provides functions for fast and correct slurping and spewing. All functions are optionally exported.
+
+=head1 FUNCTIONS
+
+=head2 read_text($filename, $encoding, %options)
+
+Reads file C<$filename> into a scalar and decodes it from C<$encoding> (which defaults to UTF-8). Can optionally take this named argument:
+
+=over 4
+
+=item * crlf
+
+This forces crlf translation on the input. The default for this argument is platform specific.
+
+=back
+
+=head2 read_binary($filename)
+
+Reads file C<$filename> into a scalar without any decoding or transformation.
+
+=head2 read_lines($filename, $encoding, %options)
+
+Reads file C<$filename> into a list/array after decoding from C<$encoding>. By default it returns this list. Can optionally take this named argument:
+
+=over 4
+
+=item * chomp
+
+C<chomp> the lines.
+
+=back
+
+=head2 read_dir($dirname, %options)
+
+Open C<dirname> and return all entries except C<.> and C<..>. Can optionally take this named argument:
+
+=over 4
+
+=item * prefix
+
+This will prepend C<$dir> to the entries
+
+=back
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Path::Tiny>
+
+A minimalistic abstraction not only around IO but also paths.
+
+=item * L<File::Slurp>
+
+Another file slurping tool.
+
+=back
+
+=head1 AUTHOR
+
+Leon Timmermans <leont@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Leon Timmermans.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
